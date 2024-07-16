48b49c14f8ab7b126c6012c08a1595432cae3806
CryNoMore <nvnsngh1008@hotmail.com>
2024-07-16 17:08:58 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import json
 import requests
+import argparse
+from urllib.parse import urlparse, urlunparse
 
 # Load Postman collection
 def load_postman_collection(collection_path):
@@ -7,50 +9,91 @@ def load_postman_collection(collection_path):
         collection = json.load(file)
     return collection
 
-# Resolve variables in the Postman collection
-def resolve_variables(collection, variables):
-    def resolve(value):
-        for var in variables:
-            value = value.replace(f"{{{{{var['key']}}}}}", var['value'])
-        return value
-    
-    for item in collection['item']:
-        if 'request' in item:
-            request = item['request']
-            request['url']['raw'] = resolve(request['url']['raw'])
-            if 'body' in request:
-                request['body']['raw'] = resolve(request['body']['raw'])
+# Load Postman environment file
+def load_postman_environment(environment_path):
+    if environment_path:
+        with open(environment_path, 'r') as file:
+            environment = json.load(file)
+    else:
+        environment = {}
+    return environment
 
-# Send cURL request using a proxy
-def send_request(request, proxy):
+# Resolve variables from both collection and environment
+def resolve_variables(collection, environment):
+    variables = {}
+
+    # Collect variables from environment
+    if 'values' in environment:
+        for value in environment['values']:
+            variables[value['key']] = value['value']
+
+    # Collect variables from collection
+    if 'variable' in collection:
+        for variable in collection['variable']:
+            variables[variable['key']] = variable['value']
+
+    return variables
+
+# Resolve variables in a URL
+def resolve_url_variables(url, variables):
+    parsed_url = urlparse(url)
+    scheme = parsed_url.scheme if parsed_url.scheme else 'https'
+    netloc = parsed_url.netloc if parsed_url.netloc else variables.get('baseUrl', '')
+    path = parsed_url.path
+    query = parsed_url.query
+
+    # Resolve variables in netloc, path, and query
+    netloc = netloc.format(**variables)
+    path = path.format(**variables)
+    query = query.format(**variables)
+
+    # Reconstruct the URL with resolved variables
+    resolved_url = urlunparse((scheme, netloc, path, '', query, ''))
+    return resolved_url
+
+# Send HTTP request using a default proxy
+def send_request(request, variables):
     method = request['method']
-    url = request['url']['raw']
+    url = resolve_url_variables(request['url']['raw'], variables)
     headers = {header['key']: header['value'] for header in request.get('header', [])}
-    data = request.get('body', {}).get('raw', '')
+    data = None
+
+    if 'body' in request:
+        mode = request['body']['mode']
+        if mode == 'raw':
+            data = request['body']['raw'].format(**variables)
+        elif mode == 'urlencoded':
+            data = {item['key']: item['value'].format(**variables) for item in request['body']['urlencoded']}
+        elif mode == 'formdata':
+            data = {item['key']: item['value'].format(**variables) for item in request['body']['formdata']}
+
+    proxy = 'http://127.0.0.1:8080'  # Define your default proxy URL here
 
     proxies = {
         "http": proxy,
         "https": proxy
     }
 
-    response = requests.request(method, url, headers=headers, data=data, proxies=proxies)
-    return response
+    try:
+        requests.request(method, url, headers=headers, data=data, proxies=proxies)
+    except requests.exceptions.MissingSchema as e:
+        print(f"Error sending request: {e}")
 
 # Main function
 def main():
-    collection_path = 'path_to_your_postman_collection.json'
-    proxy = 'http://127.0.0.1:8080'
+    parser = argparse.ArgumentParser(description='Send Postman collection requests.')
+    parser.add_argument('collection', type=str, help='Path to the Postman collection JSON file')
+    parser.add_argument('--environment', type=str, help='Path to the Postman environment JSON file (optional)')
+    args = parser.parse_args()
 
-    collection = load_postman_collection(collection_path)
-    variables = collection.get('variable', [])
-    resolve_variables(collection, variables)
+    collection = load_postman_collection(args.collection)
+    environment = load_postman_environment(args.environment)
+    variables = resolve_variables(collection, environment)
 
     for item in collection['item']:
         if 'request' in item:
-            request = item['request']
-            response = send_request(request, proxy)
-            print(f"Response from {request['url']['raw']}: {response.status_code}")
-            print(response.text)
+            request = item[a'request']
+            send_request(request, variables)
 
 if __name__ == "__main__":
     main()
